4604f25479caeb4ffa52b27297f0f4049e42b43b
Brandon <hutchingsonb@gmail.com>
2021-05-25 23:23:40 -0400

    tag belongs to many products model assoc.

--- models/index.js
+++ models/index.js
@@ -23,6 +23,11 @@ Product.belongsToMany(Tag, {
 });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through: ProductTag,
+    // as: 'product_tags',
+    foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
